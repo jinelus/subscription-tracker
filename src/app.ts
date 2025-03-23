2bd58e8e25cda8c39baf6f845b570c1f6266aa86
codesofC <inelusjudelin01@gmail.com>
2025-03-23 18:52:05 -0300

    jinelus : feat add Swagger documentation and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,10 @@ import { userRouter } from './routes/user.routes';
 import { subscriptionRouter } from './routes/subscription.routes';
 import { connectToDatabase } from './db/mongodb';
 import { errorMiddleware } from './middlewares/error.middleware';
-
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './docs/swagger-output.json'
+import { arcjetMiddleware } from './middlewares/arcjet.middleware';
+import { workflowRouter } from './routes/workflow.route';
 
 
 export const app = express()
@@ -17,9 +20,20 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+//Arcjet middleware
+app.use(arcjetMiddleware)
+
+//Swagger-ui
+const options: swaggerUi.SwaggerUiOptions = {
+    explorer: true,
+    customSiteTitle: 'Subscription Tracker API',
+}
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, options))
+
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
+app.use('/api/v1/workflow', workflowRouter)
 
 app.use(errorMiddleware)
 
