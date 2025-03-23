07796a96f8dd2c34486294388be41a8d21b17a98
codesofC <inelusjudelin01@gmail.com>
2025-03-23 18:51:22 -0300

    jinelus : feat add additional environment variables for Arcjet and Qstash configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,8 +5,15 @@ const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
     DATABASE_URL: z.string(),
     PORT: z.coerce.number().default(3333),
+    SERVER_URL: z.string(),
     JWT_SECRET: z.string(),
     JWT_EXPIRES_IN: z.string(),
+    ARCJET_KEY: z.string(),
+    ARCJET_ENV: z.string(),
+    QSTASH_URL: z.string(),
+    QSTASH_TOKEN: z.string(),
+    QSTASH_CURRENT_SIGNING_KEY: z.string(),
+    QSTASH_NEXT_SIGNING_KEY: z.string()
 })
 
 const _env = envSchema.safeParse(process.env)
